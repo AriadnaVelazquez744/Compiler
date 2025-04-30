419b03bf2607919974315bea1a0c43ad58d51dca
AriadnaVelazquez744 <velazquezreyariadna@gmail.com>
2025-04-30 09:00:37 -0400

    organizar archivos de análisis semántico

--- ast/TestSemantic.cpp
+++ ast/TestSemantic.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "AST.hpp"
-#include "SemanticAnalyzer.hpp"
+#include "../include/AST.hpp"
+#include "../include/SemanticAnalyzer.hpp"
 
 void testFunctionSemantics() {
     // Test 1: Parámetros duplicados

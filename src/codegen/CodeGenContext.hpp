cf659982abc401aed47c28f1bb183824ce8ef1e4
AriadnaVelazquez744 <velazquezreyariadna@gmail.com>
2025-06-10 02:50:53 -0400

    primera versión de las funciones auxiliares del manejo de scopes

--- src/codegen/CodeGenContext.hpp
+++ src/codegen/CodeGenContext.hpp
@@ -9,7 +9,9 @@
 #include <map>
 #include <vector> // <-- Needed for value stack
 
+// Forward declarations
 class ASTNode;
+class FunctionDeclarationNode;
 
 // This class wraps all LLVM state and IR generation context
 class CodeGenContext {
@@ -25,4 +27,37 @@ public:
 
     void generateCode(std::vector<ASTNode*>& root);                  // Main entry: triggers IR generation
     void dumpIR(const std::string& filename = "hulk-low-code.ll"); // Dumps IR to .ll file
+
+    // Scopes
+    std::vector<std::map<std::string, llvm::Value*>> localScopes;
+    std::vector<std::map<std::string, FunctionDeclarationNode*>> functionScopes;
+
+    // Var Scopes
+    void pushVarScope() { localScopes.emplace_back(); }
+    void popVarScope()  { localScopes.pop_back(); }
+    void addLocal(const std::string& name, llvm::Value* val) {
+        if (!localScopes.empty()) localScopes.back()[name] = val;
+    }
+    llvm::Value* lookupLocal(const std::string& name) const {
+        for (auto it = localScopes.rbegin(); it != localScopes.rend(); ++it) {
+            auto found = it->find(name);
+            if (found != it->end()) return found->second;
+        }
+        return nullptr;
+    }
+
+    // Func Scopes
+    void pushFuncScope() { functionScopes.emplace_back(); }
+    void popFuncScope()  { functionScopes.pop_back(); }
+    void addFuncDecl(const std::string& name, FunctionDeclarationNode* decl) {
+        if (!functionScopes.empty()) functionScopes.back()[name] = decl;
+    }
+    FunctionDeclarationNode* lookupFuncDecl(const std::string& name) const {
+        for (auto it = functionScopes.rbegin(); it != functionScopes.rend(); ++it) {
+            auto found = it->find(name);
+            if (found != it->end()) return found->second;
+        }
+        return nullptr;
+    }
+
 };
